0456e10136d5abcb60f227d57763794585721d95
LombeDev <81247194+LombeDev@users.noreply.github.com>
2026-01-13 15:49:11 +0200

    Create fpl-proxy.js

---
+++ netlify/functions/fpl-proxy.js
@@ -0,0 +1,28 @@
+const https = require('https');
+
+exports.handler = async (event) => {
+    const endpoint = event.queryStringParameters.endpoint;
+    const url = `https://fantasy.premierleague.com/api/${endpoint}`;
+
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let data = '';
+            res.on('data', (chunk) => data += chunk);
+            res.on('end', () => {
+                resolve({
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "application/json"
+                    },
+                    body: data
+                });
+            });
+        }).on('error', (e) => {
+            resolve({
+                statusCode: 500,
+                body: JSON.stringify({ error: e.message })
+            });
+        });
+    });
+};
