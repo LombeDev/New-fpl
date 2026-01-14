06df52d20478473cbd7b3eb6e5ce9729c45e8d1b
LombeDev <81247194+LombeDev@users.noreply.github.com>
2026-01-14 11:06:36 +0200

    Update fpl-proxy.js

--- netlify/functions/fpl-proxy.js
+++ netlify/functions/fpl-proxy.js
@@ -26,3 +26,30 @@ exports.handler = async (event) => {
         });
     });
 };
+
+
+
+
+// netlify/functions/fpl-proxy.js
+const axios = require('axios');
+
+exports.handler = async (event) => {
+    const path = event.path.replace('/.netlify/functions/fpl-proxy', '');
+    const url = `https://fantasy.premierleague.com/api${path}${event.rawQuery ? '?' + event.rawQuery : ''}`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Referer': 'https://fantasy.premierleague.com/'
+            }
+        });
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+            body: JSON.stringify(response.data)
+        };
+    } catch (error) {
+        return { statusCode: error.response ? error.response.status : 500, body: error.message };
+    }
+};
